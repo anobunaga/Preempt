97db727f94f10d8787e7e08ba70e1eb7200ff90e
Harold <haroldmo23@gmail.com>
2025-12-13 16:18:48 -0800

    Separate API and weather data collection #12
    Separating API and weather data collection into two processes. This will allow us to independently
    scale each if we need to do so. Also, if one goes down, the other is still up and running. Also
    adding configs as yaml so we can easily reference and set things in the future

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module preempt
 go 1.19
 
 require github.com/go-sql-driver/mysql v1.6.0
+
+require gopkg.in/yaml.v3 v3.0.1
---
+++ internal/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"gopkg.in/yaml.v3"
+)
+
+var (
+	instance *Config
+	once     sync.Once
+)
+
+// Config - can/will add more later
+type Config struct {
+	Weather struct {
+		MonitoredFields []string `yaml:"monitored_fields"`
+	} `yaml:"weather"`
+}
+
+func Load(configPath string) (*Config, error) {
+	var err error
+	once.Do(func() {
+		instance = &Config{}
+
+		data, readErr := os.ReadFile(configPath)
+		if readErr != nil {
+			err = fmt.Errorf("failed to read config file %s: %w", configPath, readErr)
+			return
+		}
+
+		if parseErr := yaml.Unmarshal(data, instance); parseErr != nil {
+			err = fmt.Errorf("failed to parse config: %w", parseErr)
+			return
+		}
+
+		if validateErr := instance.validate(); validateErr != nil {
+			err = validateErr
+			return
+		}
+	})
+
+	return instance, err
+}
+
+func Get() *Config {
+	if instance == nil {
+		panic("config not loaded - call config.Load() first")
+	}
+	return instance
+}
+
+func (c *Config) validate() error {
+	if len(c.Weather.MonitoredFields) == 0 {
+		return fmt.Errorf("weather.monitored_fields cannot be empty")
+	}
+	return nil
+}
